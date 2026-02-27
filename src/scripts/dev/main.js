79e6b199c1a3c852a3021a38742a941807938d15
belovada <belovada20@gmail.com>
2026-02-27 23:03:38 +0700

    add swiper

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -8,4 +8,63 @@
     navToggle.addEventListener("click", function(){
         root.classList.toggle("show-nav");
     });
+
+    const eventPP = document.querySelector("#js-eventPP");
+
+    if (eventPP){
+        const eventOpenBtn=document.querySelector("#js-eventOpenBtn");
+
+        const closeEventPP = function (event){
+
+            function close(){
+                document.removeEventListener("keyup", closeEventPP);
+                eventPP.removeEventListener("click", closeEventPP);
+
+                root.classList.remove("show-event-popup");
+            };
+
+            switch(event.type){
+                case "keyup":
+                    if(event.key==="Escape") close();
+                break;
+                case "click":
+                    if (event.target===this || event.target.classList.contains("js-ppCloseBtn")){
+                        close();
+                        break;
+                    }
+            }
+
+        };
+
+        eventOpenBtn.addEventListener("click", function(){
+            root.classList.add("show-event-popup");
+
+            document.addEventListener("keyup", closeEventPP);
+            eventPP.addEventListener("click", closeEventPP);
+        });
+
+    }
+
+    const swipers = document.querySelectorAll(".js-swiper");
+
+    swipers.forEach(function (swpr) {
+    new Swiper(swpr, {
+            updateOnWindowResize: true,
+            slidesPerView: "auto",
+            freeMode: true,
+            spaceBetween: 0,
+            speed: 500,
+            grabCursor: true,
+            pagination: {
+            el: swpr.querySelector(".swiper-pagination"),
+            clickable: true
+            },
+            navigation: {
+            nextEl: swpr.querySelector(".swiper-arrow-next"),
+            prevEl: swpr.querySelector(".swiper-arrow-prev"),
+            disabledClass: "arrow--disabled"
+            }
+        });
+    });
+
 })();
